e68f04afa5844ab79653f0981182b64358747fb3
ardasar94 <sarihanarda@gmail.com>
2021-05-03 13:17:36 +0300

    Home Index Filters

--- src/Web/Services/HomeViewModelService.cs
+++ src/Web/Services/HomeViewModelService.cs
@@ -23,10 +23,11 @@ namespace Web.Services
             _categoryRepository = categoryRepository;
             _authorRepository = authorRepository;
         }
-        public async Task<HomeIndexViewModel> GetHomeIndexViewModel()
+        public async Task<HomeIndexViewModel> GetHomeIndexViewModel(int? categoryId, int? authorId)
         {
-            var spec = new ProductsWithAuthorSpecification();
+            var spec = new ProductsWithAuthorSpecification(categoryId, authorId);
             var products = await _productRepository.ListAsync(spec);
+
             var vm = new HomeIndexViewModel()
             {
                 Products = products.Select(x => new ProductViewModel() { 
@@ -34,7 +35,7 @@ namespace Web.Services
                     Name = x.Name,
                     PictureUri = x.PictureUri,
                     Price = x.Price,
-                    AuthorName = x.Author.FullName
+                    AuthorName = x.Author?.FullName
                 }).ToList(),
                 Authors = await GetAuthors(),
                 Categories = await GetCategories()
